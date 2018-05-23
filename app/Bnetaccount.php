c1005b277cfc21a5d9b46f4ba8a37258008ac4b6
Timo Bussian <tb@imagearts.de>
2018-05-23 16:20:56 +0200

    Refactored whole Api Process into 1 Method

--- app/Bnetaccount.php
+++ app/Bnetaccount.php
@@ -4,14 +4,15 @@ namespace App;
 
 use Auth;
 
+use Storage;
+
 class Bnetaccount extends Model
 {
     public function User() {
         return $this->belongsTo(User::class);
     }
 
-    
-    public static function getbnetAcc($accname = "") {
+    public static function getbnetapiData($accname = "", $type = 2) {
         
         if(env('OW_API_URL', false) && env('OW_API_PORT', false)) {
             $apiurl = env('OW_API_URL', false);
@@ -33,7 +34,64 @@ class Bnetaccount extends Model
             $data = @file_get_contents($url, false, $context);
 
             if(!empty($data)) {
-                return $data;
+                
+                $decode = json_decode($data, true);
+
+                $ranks = array(
+                    'bronze' => '120px-Badge_1_Bronze.png',
+                    'silver' => '120px-Badge_2_Silver.png',
+                    'gold' => '120px-Badge_3_Gold.png',
+                    'platinum' => '120px-Badge_4_Platinum.png',
+                    'diamond' => '120px-Badge_5_Diamond.png',
+                    'master' => '120px-Badge_6_Master.png',
+                    'grandmaster' => '120px-Badge_7_Grandmaster.png',
+                    'top500' => '120px-Badge_8_Top_500.png'
+                );
+    
+                $rating = $decode['eu']['stats']['competitive']['overall_stats']['comprank'];
+                $tier = $ranks[$decode['eu']['stats']['competitive']['overall_stats']['tier']];
+                $winrate = $decode['eu']['stats']['competitive']['overall_stats']['win_rate'];
+                $avatar = $decode['eu']['stats']['competitive']['overall_stats']['avatar'];
+    
+                if(!empty($avatar)) {
+                    $picture = @file_get_contents($avatar);
+                    if(!empty($picture)) {
+                        $filename = @array_pop(explode('/', $avatar));
+                        $bla = Storage::put('public/avatar/'. $filename .'', $picture);
+                    }
+                }
+                
+                if($type == 1) {
+                    $bnetadd = Bnetaccount::create([
+                        'bnetaccount' => request('bnetaccount'),
+                        'user_id' => auth()->id(),
+                        'rating' => $rating,
+                        'tier' => $tier,
+                        'winrate' => $winrate,
+                        'avatar' => "storage\avatar\\".$filename,
+                        'statscache' => $data
+                    ]);
+
+                    if($bnetadd && $bla) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    $bnetupdate = Bnetaccount::where('user_id', auth()->id())
+                    ->update([
+                    'rating' => $rating,
+                    'tier' => $tier,
+                    'winrate' => $winrate,
+                    'avatar' => "storage\avatar\\".$filename,
+                    'statscache' => $data]);
+
+                    if($bnetupdate && $bla) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
             } else {
                 return false;
             }
--- app/Http/Controllers/BattlenetController.php
+++ app/Http/Controllers/BattlenetController.php
@@ -10,132 +10,38 @@ use Illuminate\Support\Facades\Storage;
 
 class BattlenetController extends Controller
 {
-    public function store() {
+    public function store() 
+    {
         
         $this->validate(request(), [
             'bnetaccount' => 'required'
         ]);
 
-        $data = Bnetaccount::getbnetAcc(request('bnetaccount'));
+        $bla = Bnetaccount::getbnetapiData(request('bnetaccount'), 1);
 
-        if(!empty($data)) 
-        {
-
-            $decode = json_decode($data, true);
-
-            $ranks = array(
-                'bronze' => '120px-Badge_1_Bronze.png',
-                'silver' => '120px-Badge_2_Silver.png',
-                'gold' => '120px-Badge_3_Gold.png',
-                'platinum' => '120px-Badge_4_Platinum.png',
-                'diamond' => '120px-Badge_5_Diamond.png',
-                'master' => '120px-Badge_6_Master.png',
-                'grandmaster' => '120px-Badge_7_Grandmaster.png',
-                'top500' => '120px-Badge_8_Top_500.png'
-            );
-
-            $rating = $decode['eu']['stats']['competitive']['overall_stats']['comprank'];
-            $tier = $ranks[$decode['eu']['stats']['competitive']['overall_stats']['tier']];
-            $winrate = $decode['eu']['stats']['competitive']['overall_stats']['win_rate'];
-            $avatar = $decode['eu']['stats']['competitive']['overall_stats']['avatar'];
-
-            if(!empty($avatar)) {
-                $picture = @file_get_contents($avatar);
-                if(!empty($picture)) {
-                    $filename = @array_pop(explode('/', $avatar));
-                    $bla = Storage::put('public/avatar/'. $filename .'', $picture);
-                }
-            }
-
-            $bnetadd = Bnetaccount::create([
-                'bnetaccount' => request('bnetaccount'),
-                'user_id' => auth()->id(),
-                'rating' => $rating,
-                'tier' => $tier,
-                'winrate' => $winrate,
-                'avatar' => "storage\avatar\\".$filename,
-                'statscache' => $data
-            ]);
-            
-            if($bla && $bnetadd) {
-                session()->flash('message', 'Bnetaccount sucessfully added!');
-            } else {
-                session()->flash('message', 'Bnetaccount not added!');
-            }
-            
-            return redirect('/games');
-
-        } else 
-        {
-
-            session()->flash('message', 'Bnetaccount not added or API not avaiable!');
-
-            return redirect('/games');
+        if($bla) {
+            session()->flash('message', 'Bnetaccount sucessfully added!');
+        } else {
+            session()->flash('message', 'Bnetaccount not added - Wrong format or API DOWN!');
         }
         
+        return redirect('/games');
+
     }
 
     public function refresh() 
     {   
 
-        $data = Bnetaccount::getbnetAcc();
+        $bla = Bnetaccount::getbnetapiData();
 
-        if(!empty($data)) 
+        if($bla) 
         {
-
-            $decode = json_decode($data, true);
-
-            $ranks = array(
-                'bronze' => '120px-Badge_1_Bronze.png',
-                'silver' => '120px-Badge_2_Silver.png',
-                'gold' => '120px-Badge_3_Gold.png',
-                'platinum' => '120px-Badge_4_Platinum.png',
-                'diamond' => '120px-Badge_5_Diamond.png',
-                'master' => '120px-Badge_6_Master.png',
-                'grandmaster' => '120px-Badge_7_Grandmaster.png',
-                'top500' => '120px-Badge_8_Top_500.png'
-            );
-
-            $rating = $decode['eu']['stats']['competitive']['overall_stats']['comprank'];
-            $tier = $ranks[$decode['eu']['stats']['competitive']['overall_stats']['tier']];
-            $winrate = $decode['eu']['stats']['competitive']['overall_stats']['win_rate'];
-            $avatar = $decode['eu']['stats']['competitive']['overall_stats']['avatar'];
-            
-
-
-            if(!empty($avatar)) {
-                $picture = @file_get_contents($avatar);
-                if(!empty($picture)) {
-                    $filename = @array_pop(explode('/', $avatar));
-                    $bla = Storage::put('public/avatar/'. $filename .'', $picture);
-                }
-            }
-
-            $bnetupdate = Bnetaccount::where('user_id', auth()->id())
-            ->update([
-            'rating' => $rating,
-            'tier' => $tier,
-            'winrate' => $winrate,
-            'avatar' => "storage\avatar\\".$filename,
-            'statscache' => $data]);
-
-
-            if($bla) 
-            {
-                session()->flash('message', 'Statistics successfully updatet!');
-            } else {
-                session()->flash('message', 'Statistics successfully updatet, couldn´t update Avatar!');
-            }
-            
-
-            return redirect('/games');
-
-        } else 
-        {
-            session()->flash('message', 'Statistics not updatet API not avaiable!');
-
-            return redirect('/games');
+            session()->flash('message', 'Statistics successfully updatet!');
+        } else {
+            session()->flash('message', 'Statistics could not be updatet!');
         }
+        
+        return redirect('/games');
 
     }
     
